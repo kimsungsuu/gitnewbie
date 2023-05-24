9e00f70b0af089661505d0b8900110df771d425d
Kim sungsu <ksungsu>
2023-05-24 15:52:50 +0900

    create gitnewbie first commit

---
+++ src/main/java/com/gitnewbie/GitnewbieApplication.java
@@ -0,0 +1,13 @@
+package com.gitnewbie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitnewbieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitnewbieApplication.class, args);
+	}
+
+}
